e1138b1a39bd300c9ab0df6bc1a2df16cd41b8f0
Mindy Preston <meetup@yomimono.org>
2015-03-17 15:34:58 +0000

    work w/arp that wants a clock argument

--- arp_tester/unikernel.ml
+++ arp_tester/unikernel.ml
@@ -3,28 +3,28 @@ open Lwt
 open OUnit
 
 module Main (C: CONSOLE) (K: KV_RO) = struct
-  module P = Netif.Make(K)(OS.Time) 
+  module P = Netif.Make(K)(OS.Time)
   module E = Ethif.Make(P)
-  module I = Ipv4.Make(E)
+  module I = Ipv4.Make(E)(Clock)(OS.Time)
   module A = Arpv4.Make(E)
   module U = Udp.Make(I)
 
-      (* unfortunately, arp isn't exposed in wire_structs nor in Arpv4, so 
+      (* unfortunately, arp isn't exposed in wire_structs nor in Arpv4, so
          we reproduce it here, nonoptimally *)
-      cstruct arp {                                                                 
-      uint8_t dst[6];                                                             
-      uint8_t src[6];                                                             
-      uint16_t ethertype;                                                         
-      uint16_t htype;                                                             
-      uint16_t ptype;                                                             
-      uint8_t hlen;                                                               
-      uint8_t plen;                                                               
-      uint16_t op;                                                                
-      uint8_t sha[6];                                                             
-      uint32_t spa;                                                               
-      uint8_t tha[6];                                                             
-      uint32_t tpa                                                                
-    } as big_endian              
+      cstruct arp {
+      uint8_t dst[6];
+      uint8_t src[6];
+      uint16_t ethertype;
+      uint16_t htype;
+      uint16_t ptype;
+      uint8_t hlen;
+      uint8_t plen;
+      uint16_t op;
+      uint8_t sha[6];
+      uint32_t spa;
+      uint8_t tha[6];
+      uint32_t tpa
+    } as big_endian
 
     cenum op {
       Op_request = 1;
@@ -63,23 +63,23 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
     (* use E.input to get tcpip to parse this for us *)
     (* don't need to worry about mac filtering in ethif because we're
        looking for something that will have dst = broadcast *)
-    E.input ~arpv4:(fun buf -> (if is_arp_probe buf && came_from_us buf then 
-                                  is_arp := true; Lwt.return_unit)) 
+    E.input ~arpv4:(fun buf -> (if is_arp_probe buf && came_from_us buf then
+                                  is_arp := true; Lwt.return_unit))
       ~ipv4:not_arp ~ipv6:not_arp e packet >>= fun () -> Lwt.return !is_arp
 
   let send_traffic u =
-    U.write ~source_port:1000 ~dest_ip:target ~dest_port:1024 u 
+    U.write ~source_port:1000 ~dest_ip:target ~dest_port:1024 u
       (Cstruct.create 0) >>= fun () ->
     Lwt.return `Success
 
   let test_send_arps p e u =
-    let try_connecting _context = 
-      U.write ~source_port:1000 ~dest_ip:silent_host 
+    let try_connecting _context =
+      U.write ~source_port:1000 ~dest_ip:silent_host
         ~dest_port:1024 u (Cstruct.create 0) >>= fun () ->
       Lwt.return (`Failure "Sent a UDP packet for a host which can't have been
                       in the ARP cache")
     in
-    let timeout_then_succeed _context = 
+    let timeout_then_succeed _context =
       OS.Time.sleep 1.0 >>= fun () ->
       (* check to make sure we wrote an ARP probe *)
       match (P.get_written p) with
@@ -94,14 +94,14 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
       timeout_then_succeed ();
     ]
 
-  let test_garp_was_read p e u = 
+  let test_garp_was_read p e u =
     let timeout_then_fail _context =
       let timeout = 1.0 in
       OS.Time.sleep timeout >>= fun () ->
       (* make sure the failure is because we wrote an arp request packet *)
       match P.get_written p with
       | [] -> Lwt.return (`Failure "Timed out, although we didn't write anything?")
-      | l -> 
+      | l ->
         is_arp_request e (List.hd (List.rev l)) >>= function
         | true -> Lwt.return (`Failure "sent an arp probe for something that
           just GARPed")
@@ -120,18 +120,33 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
 
   let test_arp_aged_out p e u =
     (* attempt a connection, which we expect not to succeed *)
-    let arp_age = 15.0 (* set to whatever the arp aging interval is; would be
+    let arp_age = 65.0 (* set to whatever the arp aging interval is; would be
                           nice to have this visible *) in
-    let try_connecting _context = 
+    let try_connecting _context =
       (* make sure we were cool to write initially *)
       U.write ~source_port:1000 ~dest_ip:target ~dest_port:1024 u
-        (Cstruct.create 0) >>= fun () -> 
+        (Cstruct.create 0) >>= fun () ->
       OS.Time.sleep arp_age >>= fun () ->
       U.write ~source_port:1000 ~dest_ip:target ~dest_port:1024 u
-        (Cstruct.create 0) >>= fun () -> Lwt.return (`Failure "Sent a packet
-          when we should've had no ARP entry for the destination")
+        (Cstruct.create 0) >>= fun () ->
+      (* spoo out the last packet sent for debugging *)
+      let last_sent = (List.hd (List.rev (P.get_written p))) in
+      match (Cstruct.len last_sent) with
+      | 0 ->
+        Printf.printf "Okay, this is obviously bananas.";
+        List.iter (fun d -> Printf.printf "packet:\n"; Cstruct.hexdump d)
+          (P.get_written p);
+        Lwt.return (`Failure "Sent a packet with no contents
+                                     when we had no ARP entry for the
+                                     destination.")
+      | n ->
+        let b = Buffer.create (Cstruct.len last_sent) in
+        Cstruct.hexdump_to_buffer b last_sent;
+        Lwt.return (`Failure (Printf.sprintf "Sent a packet %s with length %d
+          when we should've had no ARP entry for the destination"
+                                (Buffer.contents b) n))
     in
-    let timeout_then_succeed _context = 
+    let timeout_then_succeed _context =
       OS.Time.sleep (arp_age +. 1.0) >>= fun () ->
       (* check to make sure we wrote an ARP probe *)
       match (P.get_written p) with
@@ -147,18 +162,22 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
     ]
 
   let test_queries_retried p e u =
-    let try_connecting _context = 
-      U.write ~source_port:1000 ~dest_ip:silent_host 
-        ~dest_port:1024 u (Cstruct.create 0) >>= fun () ->
+    let try_connecting _context =
+      try_lwt U.write ~source_port:1000 ~dest_ip:silent_host
+        ~dest_port:1024 u (Cstruct.create 0)
+      >>= fun () ->
       Lwt.return (`Failure "Sent a UDP packet for a host which can't have been
                       in the ARP cache")
+      (* with I.Routing.No_route_to_destination_address silent_host ->
+         Lwt.return `Success *)
+with Not_found -> Lwt.return `Success
     in
-    let timeout_then_succeed _context = 
+    let timeout_then_succeed _context =
       let rec first_k_are_arp_requests l (k : int) =
         match l, k with
         | _, 0 -> Lwt.return true
         | [], _ -> Lwt.return false
-        | p :: more, k when (k > 0) -> 
+        | p :: more, k when (k > 0) ->
           is_arp_request e p >>= fun this_one ->
           first_k_are_arp_requests more (k - 1) >>= fun others ->
           Lwt.return (this_one && others)
@@ -171,10 +190,10 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
       (* check to make sure we wrote three ARP probes *)
       match (P.get_written p) with
       | [] -> Lwt.return (`Failure "Wrote nothing when should've ARP probed")
-      | l -> first_k_are_arp_requests (List.rev l) 
+      | l -> first_k_are_arp_requests (List.rev l)
                (int_of_float number_of_retries) >>= function
         | true -> Lwt.return `Success
-        | false -> Lwt.return 
+        | false -> Lwt.return
                      (`Failure (Printf.sprintf "Last %d sent packets weren't ARP probes"
                         (int_of_float number_of_retries)))
     in
@@ -186,7 +205,7 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
   let start c k =
 
     let or_error c name fn t =
-      fn t >>= function 
+      fn t >>= function
       | `Error e -> fail (Failure ("Error starting " ^ name))
       | `Ok t -> return t
     in
@@ -207,8 +226,8 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
       Lwt.return (p, e, i, u)
     in
     let play_pcap (p, e, i, u) =
-      P.listen p (E.input 
-                    ~arpv4:(fun buf -> I.input_arpv4 i buf) 
+      P.listen p (E.input
+                    ~arpv4:(fun buf -> I.input_arpv4 i buf)
                     ~ipv4:(fun buf -> Lwt.return_unit)
                     ~ipv6:(fun buf -> Lwt.return_unit) e
                  ) >>= fun () ->
@@ -218,39 +237,47 @@ module Main (C: CONSOLE) (K: KV_RO) = struct
        for that address in the arp cache now *)
     (* send a udp packet to that address, which will result in an attempt to
        resolve the address on the ARP layer.  If the thread returns, all's well. *)
-    (* 
+    (*
 x 1) we age out arp entries after some amount of time
 x 2) we update arp entries in the presence of new information
 x 3) we send out arp probes when trying to resolve unknown addresses
-x 4) we retry arp probes a predictable number of times
+x 4) we retry arp probes a predictable number of timesen
 5) we stop retrying arp probes once one has succeeded
 x 6) on successful reception of an arp reply, we don't unnecessarily delay our response
 *)
     (* we really should be doing each of these with a fresh pcap_netif;
        otherwise we run the risk of contaminating state between runs *)
+    C.log c "testing that arp probes are sent for entries that shouldn't be in
+    the cache...";
     setup_iface file ip nm >>= fun send_arp_test_stack ->
     play_pcap send_arp_test_stack >>= fun (p, e, i, u) ->
     test_send_arps p e u >>= fun result ->
     assert_equal ~printer `Success result;
-    
-    setup_iface ~timing:(Some 1.0) file ip nm >>= fun (p, e, i, u) ->
+
+    C.log c "testing that once a response is received, a query thread returns
+    that response immediately...";
+    setup_iface file ip nm >>= fun (p, e, i, u) ->
     Lwt.pick [
-      send_traffic u;
-      (play_pcap (p, e, i, u) >>= fun query_stack -> 
-      Lwt.return (`Failure "return_on_reply: playback thread terminated first"))
+      (play_pcap (p, e, i, u) >>= fun query_stack ->
+      Lwt.return (`Failure "return_on_reply: playback thread terminated
+      first"));
+      send_traffic u; (* these don't appear to actually be interleaved *)
     ] >>= fun result ->
     assert_equal ~printer `Success result;
 
+    C.log c "testing that probes are retried...";
     setup_iface file ip nm >>= fun arp_query_retry_stack ->
     play_pcap arp_query_retry_stack >>= fun (p, e, i, u) ->
     test_queries_retried p e u >>= fun result ->
     assert_equal ~printer `Success result;
 
+    C.log c "testing that gratuitous arps are recorded in the cache...";
     setup_iface file ip nm >>= fun garp_reads_test_stack ->
     play_pcap garp_reads_test_stack >>= fun (p, e, i, u) ->
     test_garp_was_read p e u >>= fun result ->
     assert_equal ~printer `Success result;
 
+    C.log c "testing that entries are aged out...";
     setup_iface file ip nm >>= fun arp_aging_test_stack ->
     play_pcap send_arp_test_stack >>= fun (p, e, i, u) ->
     test_arp_aged_out p e u >>= fun result ->
